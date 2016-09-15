743fa209fa055ee846faed455a89eb73e2638cad
Niki Ahlskog <niki.ahlskog@gmail.com>
2016-09-15 20:17:09 +0300

    layout coming

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -7,6 +7,26 @@
    	  View::make('home.html');
     }
 
+    public static function stories(){
+      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+      View::make('stories.html');
+    }
+
+    public static function topten(){
+      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+      View::make('topten.html');
+    }
+
+    public static function rekisteroidy(){
+      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+      View::make('rekisteroidy.html');
+    }
+
+    public static function kirjaudu(){
+      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+      View::make('kirjaudu.html');
+    }
+
     public static function sandbox(){
       // Testaa koodiasi täällä
       echo 'Hello World!';
--- config/routes.php
+++ config/routes.php
@@ -4,6 +4,22 @@
     HelloWorldController::index();
   });
 
+  $routes->get('/selaa', function() {
+    HelloWorldController::stories();
+  });
+
+  $routes->get('/topten', function() {
+    HelloWorldController::topten();
+  });
+
+  $routes->get('/rekisteroidy', function() {
+    HelloWorldController::rekisteroidy();
+  });
+
+  $routes->get('/kirjaudu', function() {
+    HelloWorldController::kirjaudu();
+  });
+
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
