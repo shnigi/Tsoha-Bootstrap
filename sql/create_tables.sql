57b9acdb0aa745d4ecb0fcf3af1f55ff6c96bec1
Niki Ahlskog <niki.ahlskog@gmail.com>
2016-09-16 16:17:41 +0300

    database scripts coming

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,21 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE appuser(
+  id SERIAL PRIMARY KEY,
+  username varchar(50) NOT NULL FOREIGN KEY,
+  password varchar(50) NOT NULL,
+  registerdate timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE stories(
+  id SERIAL PRIMARY KEY,
+  points INTEGER,
+  story varchar(5000) NOT NULL,
+  updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  createdby varchar(50) REFERENCES appuser(username)
+);
+
+CREATE TABLE comments(
+  id SERIAL PRIMARY KEY,
+  comment varchar(2000),
+  updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  createdby varchar(50) REFERENCES appuser(username)
+);
